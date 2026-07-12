094c3f355a520a1973ff36befbf33f1be6777970
deepakgngwr <22f1001001@ds.study.iitm.ac.in>
2026-07-12 18:55:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-EMAIL = "22f1001001l@ds.study.iitm.ac.in"
+EMAIL = "22f1001001@ds.study.iitm.ac.in"
 AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIyZjEwMDEwMDFAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4MjYyMTM3MiwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4MzIyNjE3Mn0.jkH2qy9tZ2RQd3aL8ChmQytjZZRhMxXRjrGd4FonVRY"
 
 AIPIPE_BASE = "https://aipipe.org/openai/v1"
